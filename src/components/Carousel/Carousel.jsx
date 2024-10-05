48bb4fcd2f215b38f4a08987ddef3c16ef82fa17
Brandon Gonzalez <brajgon@gmail.com>
2024-10-05 14:39:35 -0600

    adding RSVP feature

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -9,10 +9,9 @@ function CarouselComponent() {
           src="https://via.placeholder.com/800x400"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First Slide</h3>
-          <p>This is the first slide description.</p>
-        </Carousel.Caption>
+        {/* <Carousel.Caption>
+          
+        </Carousel.Caption> */}
       </Carousel.Item>
 
       <Carousel.Item>
@@ -21,10 +20,9 @@ function CarouselComponent() {
           src="https://via.placeholder.com/800x400"
           alt="Second slide"
         />
-        <Carousel.Caption>
-          <h3>Second Slide</h3>
-          <p>This is the second slide description.</p>
-        </Carousel.Caption>
+        {/* <Carousel.Caption>
+          
+        </Carousel.Caption> */}
       </Carousel.Item>
 
       <Carousel.Item>
@@ -33,10 +31,9 @@ function CarouselComponent() {
           src="https://via.placeholder.com/800x400"
           alt="Third slide"
         />
-        <Carousel.Caption>
-          <h3>Third Slide</h3>
-          <p>This is the third slide description.</p>
-        </Carousel.Caption>
+        {/* <Carousel.Caption>
+          
+        </Carousel.Caption> */}
       </Carousel.Item>
     </Carousel>
   );
