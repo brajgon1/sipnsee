1a436ea09e07884bd735e26f88cf57a42c16691c
Brandon Gonzalez <brajgon@gmail.com>
2024-10-05 14:22:10 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CarouselComponent from './components/Carousel/Carousel';
+
+function App() {
+  return (
+    <div className="App">
+      <CarouselComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,45 @@
+import { Carousel } from 'react-bootstrap';
+
+function CarouselComponent() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First Slide</h3>
+          <p>This is the first slide description.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second Slide</h3>
+          <p>This is the second slide description.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third Slide</h3>
+          <p>This is the third slide description.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselComponent;
