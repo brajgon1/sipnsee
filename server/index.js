33e35254ad3efa6293ee9bd33f028cc1ee63216c
Brandon Gonzalez <brajgon@gmail.com>
2024-10-06 14:13:17 -0600

    adding routes and controller access in index.js - server

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ const app = express();
 const PORT = process.env.PORT || 4005;
 
 // controllers
-// const handleRSVP = require('./rsvpController')
+const handleRSVP = require('./rsvpController')
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
 // Routes
-// app.post('/rsvps', handleRSVP)
+app.post('/rsvps', handleRSVP)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
