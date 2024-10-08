edf5c584156e600bf0c5d2c9dfad1ddf6ef01864
Brandon Gonzalez <brajgon@gmail.com>
2024-10-07 21:56:43 -0600

    fixing form, added guest calculation

--- src/components/RSVP/RSVP.jsx
+++ src/components/RSVP/RSVP.jsx
@@ -1,56 +1,70 @@
 import { useState } from "react";
-import axios from 'axios'
-import './RSVP.css'
+import axios from "axios";
+import "./RSVP.css";
 
 function RSVPForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [guestCount, setGuestCount] = useState("");
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:4005/rsvp', {
+      const response = await axios.post("http://localhost:4005/rsvp", {
         name: name,
         email: email,
       });
 
       if (response.status === 201) {
-        setMessage(response.data.message); 
+        setMessage(response.data.message);
       } else {
         setMessage("Something went wrong with your RSVP.");
       }
     } catch (error) {
       console.error("Error submitting RSVP:", error);
-      setMessage("Failed to send RSVP. Please try again later.");
+      setMessage("Failed to send RSVP");
     }
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <div>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <input
-          type="email"
-          placeholder="Email"
-          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">RSVP</button>
-      {message && <p>{message}</p>}
-    </form>
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Number of Guests </label>
+          <input
+            type="number"
+            min="1"
+            max="5"
+            value={guestCount}
+            onChange={(e) => setGuestCount(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">RSVP</button>
+        {message && <p>{message}</p>}
+      </form>
+    </div>
   );
 }
 
