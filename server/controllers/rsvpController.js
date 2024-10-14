02ee1c2808d96fd9a818d0b8c03c340099f444c0
Brandon Gonzalez <brajgon@gmail.com>
2024-10-13 21:55:46 -0600

    got nodemailer working

--- server/controllers/rsvpController.js
+++ server/controllers/rsvpController.js
@@ -1,4 +1,5 @@
 const { supabase } = require("../config/supabaseClient");
+const { sendMail } = require("../config/nodemailerConfig");
 
 const handleRSVP = async (req, res) => {
   const { name, email, guest_count } = req.body;
@@ -8,8 +9,7 @@ const handleRSVP = async (req, res) => {
     const { data, error } = await supabase
       .from("rsvps")
       .insert([{ name, email, guest_count }]);
-      console.log("Inserting into rsvps:", { name, email, guest_count });
-
+    console.log("Inserting into rsvps:", { name, email, guest_count });
 
     if (error) {
       console.error(error);
@@ -18,7 +18,31 @@ const handleRSVP = async (req, res) => {
         .json({ message: "Failed to save RSVP. Please try again" });
     }
 
-    res.status(201).json({ message: "RSVP saved successfully" });
+    const subject = "RSVP Confirmation";
+    const message = `
+    <h1>Thank you for RSVPing, ${name}</h1>
+    <p>We have received your RSVP for ${guest_count} guest(s)</p>
+    <p>Please let us know if you have any questions or concerns.</p>
+    <p>The event is <strong>November 2, 2024</strong> at <strong>4544 W. Harvest Moon Dr. South Jordan, UT 84009 - Conference Room A</strong> from <strong>2:30 PM to 4:30 PM</strong></p>
+    <p>We look forward to seeing you at the Sip & See!</p>
+    `;
+
+    try {
+      await sendMail(email, subject, message);
+      console.log("Email sent successfully:", email);
+    } catch (emailError) {
+      console.error("Failed sending confirmation Email:", emailError);
+      return res.status(500).json({
+        message:
+          "RSVP saved, but there was an error sending the confirmation email",
+      });
+    }
+
+    res
+      .status(201)
+      .json({
+        message: "RSVP saved successfully! A confirmation email has been sent.",
+      });
   } catch (error) {
     console.error(error);
     res
