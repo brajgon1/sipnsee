8522d19c26a16ffff61ec9e648313b9c704877a5
Brandon Gonzalez <brajgon@gmail.com>
2024-10-13 22:34:25 -0600

    fixed some mobile UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import CarouselComponent from './components/Carousel/Carousel';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import CarouselComponent from "./components/Carousel/Carousel";
 
 // double check code to see how to correctly show the carousel first and then take user to the form
 
-
 function App() {
-
   const handleRSVPSuccess = (message) => {
     console.log(message);
   };
 
   return (
     <div className="background">
-      <CarouselComponent onSubmitSuccess={handleRSVPSuccess} />
+      <div className="content">
+        <CarouselComponent onSubmitSuccess={handleRSVPSuccess} />
+      </div>
     </div>
   );
 }
