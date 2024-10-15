dcf8dd75a9c7c2455a4dab9b2e55363cbcbc6831
Brandon Gonzalez <brajgon@gmail.com>
2024-10-14 22:05:54 -0600

    fixing some CSS

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,2 @@
 
 
-function Footer() {
-  return (
-    <div class="rsvp-container">
-      <div class="rsvp-info">You're Invited to Hazel's Sip & See!</div>
-      <div class="rsvp-details">
-        November 2, 2024
-        <br />
-        2:30 PM to 4:30 PM
-        <br />
-        4544 W. Harvest Moon Dr. South Jordan, UT 84009
-        <br />
-        Conference Room A
-      </div>
-    </div>
-  )
-}
-
-export default Footer
